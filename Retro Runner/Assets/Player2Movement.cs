c3aa0f30ec5a3cb945a8b5b7e42b0b41c7f8980e
Frederico Flores Moita da Silva <118013263+TwickE@users.noreply.github.com>
2023-05-11 17:16:59 +0100

    Added the second player and the jump movement to both

--- Retro Runner/Assets/Player2Movement.cs
+++ Retro Runner/Assets/Player2Movement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player1Movement : MonoBehaviour
+public class Player2Movement : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,7 +13,7 @@ public class Player1Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown("w")) {
+        if(Input.GetKeyDown("up")) {
             GetComponent<Rigidbody2D>().velocity = new Vector3(0, 14, 0);
         }
     }
