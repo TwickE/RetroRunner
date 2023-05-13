e5ae740d48597498109f08c28540dea5e4e44ce7
Frederico Flores Moita da Silva <118013263+TwickE@users.noreply.github.com>
2023-05-13 17:23:14 +0100

    Added Idle and Run animation

---
+++ Retro Runner/Assets/Scripts/Player1Movement.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1Movement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private SpriteRenderer sprite;
+    private Animator anim;
+
+    private float dirX = 0f;
+    [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private float jumpForce = 14f;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        //Move left and right
+        dirX = Input.GetAxisRaw("HorizontalPlayer1");
+        rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
+
+        //Jumping
+        if(Input.GetButtonDown("JumpPlayer1")) {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+
+        //Calls the animation function
+        UpdateAnimationStatePlayer1();
+    }
+
+    //Toggles animations
+    private void UpdateAnimationStatePlayer1()
+    {
+        //Toggles the running animation
+        if(dirX > 0f)
+        {
+            anim.SetBool("running", true);
+            sprite.flipX = false;
+        }
+        else if(dirX < 0f)
+        {
+            anim.SetBool("running", true);
+            sprite.flipX = true;
+        }
+        else
+        {
+            anim.SetBool("running", false);
+        }
+    }
+}
